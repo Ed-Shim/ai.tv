6bc7ea2509904112eede0a36f237e978e4b65353
EdShim <eishimizu0607@gmail.com>
2025-05-03 23:47:45 -0700

    added sliding window variable to videoConfig

--- src/config/videoConfig.ts
+++ src/config/videoConfig.ts
@@ -1,3 +1,7 @@
 // Configuration for video frame capture
 export const MAX_FRAMES = 5;
 export const FRAME_CAPTURE_FPS = 1;
+
+//Configuration for chat api analytics
+const SLIDING_OVERLAP_WINDOW = 2; //must be < MAX_FRAMES
+export const SLIDING_INTERVAL_FRAME = MAX_FRAMES - SLIDING_OVERLAP_WINDOW;
